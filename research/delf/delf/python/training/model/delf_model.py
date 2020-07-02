79341e717cf71c631f5981ad50268fc8c686667e
Trigram19 <65362489+Trigram19@users.noreply.github.com>
2020-07-02 14:24:56 +0530

    Update delf_model.py

--- research/delf/delf/python/training/model/delf_model.py
+++ research/delf/delf/python/training/model/delf_model.py
@@ -28,6 +28,74 @@ reg = tf.keras.regularizers
 
 _DECAY = 0.0001
 
+class Attention(Layer):
+    def __init__(self, step_dim,
+                 W_regularizer=None, b_regularizer=None,
+                 W_constraint=None, b_constraint=None,
+                 bias=True, **kwargs):
+        self.supports_masking = True
+        self.init = initializers.get('glorot_uniform')
+
+        self.W_regularizer = regularizers.get(W_regularizer)
+        self.b_regularizer = regularizers.get(b_regularizer)
+
+        self.W_constraint = constraints.get(W_constraint)
+        self.b_constraint = constraints.get(b_constraint)
+
+        self.bias = bias
+        self.step_dim = step_dim
+        self.features_dim = 0
+        super(Attention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight((input_shape[-1],),
+                                 initializer=self.init,
+                                 name='{}_W'.format(self.name),
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
+        self.features_dim = input_shape[-1]
+
+        if self.bias:
+            self.b = self.add_weight((input_shape[1],),
+                                     initializer='zero',
+                                     name='{}_b'.format(self.name),
+                                     regularizer=self.b_regularizer,
+                                     constraint=self.b_constraint)
+        else:
+            self.b = None
+
+        self.built = True
+
+    def compute_mask(self, input, input_mask=None):
+        return None
+
+    def call(self, x, mask=None):
+        features_dim = self.features_dim
+        step_dim = self.step_dim
+
+        eij = K.reshape(K.dot(K.reshape(x, (-1, features_dim)),
+                        K.reshape(self.W, (features_dim, 1))), (-1, step_dim))
+
+        if self.bias:
+            eij += self.b
+
+        eij = K.tanh(eij)
+
+        a = K.exp(eij)
+
+        if mask is not None:
+            a *= K.cast(mask, K.floatx())
+
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+
+        a = K.expand_dims(a)
+        weighted_input = x * a
+        return K.sum(weighted_input, axis=1)
+
+    def compute_output_shape(self, input_shape):
+
 
 class AttentionModel(tf.keras.Model):
   """Instantiates attention model.
@@ -56,6 +124,7 @@ class AttentionModel(tf.keras.Model):
         name='attn_conv1')
     self.bn_conv1 = layers.BatchNormalization(axis=3, name='bn_conv1')
 
+    self.attn = Attention(512)
     # Second convolutional layer, with softplus activation.
     self.conv2 = layers.Conv2D(
         1,
@@ -68,6 +137,7 @@ class AttentionModel(tf.keras.Model):
   def call(self, inputs, training=True):
     x = self.conv1(inputs)
     x = self.bn_conv1(x, training=training)
+    x = self.attn(x)
     x = tf.nn.relu(x)
 
     score = self.conv2(x)
